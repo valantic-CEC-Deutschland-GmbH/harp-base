394233a26003d2112391864db8f8a706f071db0e
Aistė Nečiūnaitė <aiste.neciunaite@cec.valantic.com>
2024-04-26 17:57:47 +0300

    add plp

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Controller\Home;
 
-use Htmxfony\Template\TemplateBlock;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,28 +12,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_home_index')]
     public function __invoke(): Response
     {
-        $path = __DIR__ . '/../../../example-data/navigation/navigation.json';
-        var_dump($path);
-
-        $navigationData = file_get_contents($path);
-
-
-        return $this->htmxRenderBlock(
-            new TemplateBlock(
-                'HOME/header.html.twig',
-                'header',
-                [],
-            ),
-            new TemplateBlock(
-                'HOME/navigation.html.twig',
-                'navigation',
-                [ 'navigationData' => json_decode($navigationData, true, 512, JSON_THROW_ON_ERROR) ],
-            ),
-            new TemplateBlock(
-                'HOME/footer.html.twig',
-                'footer',
-                [],
-            ),
-        );
+        return $this->render('home/index.html.twig');
     }
 }
