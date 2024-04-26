a33e0d36b3d861231377a25d9e12f577dbef1f98
haukestenzel <hauke.stenzel@cec.valantic.com>
2024-04-26 15:51:38 +0200

    no-task Update IndexController to include a footer template and remove the header template.

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -13,7 +13,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_home_index')]
     public function __invoke(): Response
     {
-
         $path = __DIR__ . '/../../../example-data/navigation/navigation.json';
         var_dump($path);
 
@@ -32,8 +31,8 @@ class IndexController extends AbstractController
                 [ 'navigationData' => json_decode($navigationData, true, 512, JSON_THROW_ON_ERROR) ],
             ),
             new TemplateBlock(
-                'HOME/header.html.twig',
-                'header',
+                'HOME/footer.html.twig',
+                'footer',
                 [],
             ),
         );
