47b64b6b0e8e8d40315cc16483da1885d0a3ee2a
Aistė Nečiūnaitė <aiste.neciunaite@cec.valantic.com>
2024-04-26 18:57:49 +0300

    update index page

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -1,17 +1,36 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller\Home;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Htmxfony\Controller\HtmxControllerTrait;
+use Htmxfony\Response\HtmxResponse;
+use Htmxfony\Request\HtmxRequest;
+use Htmxfony\Template\TemplateBlock;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
+    use HtmxControllerTrait;
     #[Route('/', name: 'app_home_index')]
-    public function __invoke(): Response
+    public function index(HtmxRequest $request): HtmxResponse|Response
     {
-        return $this->render('home/index.html.twig');
+        if ($request->isHtmxRequest()) {
+            // do partial rendering
+            return $this->htmxRenderBlock(
+                new TemplateBlock(
+                    'home/index.html.twig',
+                    'main'
+                )
+            );
+        } else {
+            // render full page
+            return $this->render(
+                'home/index.html.twig'
+            );
+        }
     }
 }
