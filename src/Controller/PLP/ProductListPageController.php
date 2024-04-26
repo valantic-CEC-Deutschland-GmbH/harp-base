ccc1848deed71655a60a59a386f748e761608f14
haukestenzel <hauke.stenzel@cec.valantic.com>
2024-04-26 15:03:06 +0200

    no-task Add Htmxfony library dependencies and integrate Htmxfony features into PLP controller and templates.

--- src/Controller/PLP/ProductListPageController.php
+++ src/Controller/PLP/ProductListPageController.php
@@ -4,14 +4,24 @@ declare(strict_types = 1);
 
 namespace App\Controller\PLP;
 
+use Htmxfony\Controller\HtmxControllerTrait;
+use Htmxfony\Response\HtmxResponse;
+use Htmxfony\Template\TemplateBlock;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class ProductListPageController extends AbstractController
 {
-    #[Route('/category/:categoryId', name: 'app_plp_index')]
-    public function __invoke(): Response
+    use HtmxControllerTrait;
+    #[Route('/category/{categoryId}', name: 'app_plp_index')]
+    public function index(int $categoryId): HtmxResponse
     {
-        return $this->render('plp/index.html.twig');
+        $plpData = file_get_contents(__DIR__ . '/example-data/Products/products.json');
+
+        return $this->htmxRenderBlock(
+            new TemplateBlock(
+                'plp/block.html.twig',
+                'plp1',
+                [ 'plpData' => json_decode($plpData, true, 512, JSON_THROW_ON_ERROR) ],
+            ));
     }
 }
